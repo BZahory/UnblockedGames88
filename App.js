11d745efe64772339c7c149cdd8267ae4888aa7f
dylana62 <d_armstrong999@aol.com>
2019-05-20 12:18:22 -0400

    Add ball-moving files

--- App.js
+++ App.js
@@ -1,21 +1,6 @@
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import MoveBall from "./MoveBall";
+import AccelerometerSensor from "./AccelerometerSensor";
 
-export default class App extends React.Component {
-  render() {
-    return (
-      <View style={styles.container}>
-        <Text>Open up App.js to start working on your app!</Text>
-      </View>
-    );
-  }
-}
+export default MoveBall;
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+
