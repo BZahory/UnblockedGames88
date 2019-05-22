72f00efc84af5a45090b0b7c80c763d0f025833b
dylana62 <d_armstrong999@aol.com>
2019-05-22 11:41:47 -0400

    Add acceleration to the MoveBall code

--- MoveBall.js
+++ MoveBall.js
@@ -2,26 +2,27 @@ import React, { Component } from 'react';
 import { View, StyleSheet, Text, Dimensions } from 'react-native';
 
 
-//todo: make the ball expand and contract 
+//todo: make the ball expand and contract
 //      change the angle when the ball hits the wall - modify xSpeed or ySpeed
 class MoveBall extends React.Component {
 	constructor() {
        super();
-       this.state = { x: 50, 
+       this.state = { x: 50,
 	                  y: 50,
 					  xInc: true,
 					  yInc: true,
 					  xSpeed: 5,
 					  ySpeed: 15,
+						yAccel: 1,
 					  diameter: 60,
                       seconds: 0};
 	}
-	
+
 	timerEvent = () => {
 		//get the dimensions of the screen
 		let deviceWidth = Dimensions.get('window').width;
 		let deviceHeight = Dimensions.get('window').height;
-		
+
 		//update the current x coordinates
 		let curX = this.state.x;
 		let curXDir = this.state.xInc;
@@ -37,17 +38,19 @@ class MoveBall extends React.Component {
 				curXDir = true;
 			}
 		}
-		
+
 		//update the current y coordinates
 		let curY = this.state.y;
 		let curYDir = this.state.yInc;
 		if (curYDir) {
+			this.state.ySpeed -= this.state.yAccel;
 			curY += this.state.ySpeed;
 			if (curY > deviceHeight) {
 				curYDir = false;
 			}
 		}
 		else  {
+			this.state.ySpeed -= this.state.yAccel;
 			curY -= this.state.ySpeed;
 			if (curY < 0) {
 				curYDir = true;
@@ -56,11 +59,11 @@ class MoveBall extends React.Component {
 		//update state with local variables
         this.setState( {x: curX, y: curY, xInc: curXDir, yInc: curYDir} );
     };
-  
+
   componentDidMount() {
-    setInterval( this.timerEvent, 20 );  
+    setInterval( this.timerEvent, 20 );
   }
-  
+
   ballStyle = function(options) {
      return {
       position: "absolute",
@@ -72,13 +75,13 @@ class MoveBall extends React.Component {
 	  backgroundColor: 'red',
      }
  }
-  
+
    render() {
       return (
 	    <View style={styles.container}>
 		  <View style={styles.timerView}>
              <Text style = {styles.textCenter}>
-                  x: {round(this.state.x)} y: {round(this.state.y)} 
+                  x: {round(this.state.x)} y: {round(this.state.y)}
               </Text>
           </View>
 		  <View style={this.ballStyle()}>
@@ -103,7 +106,7 @@ const styles = StyleSheet.create({
     flex: 2,
     alignItems: 'center',
     justifyContent: 'center',
-    
+
   },
   buttonView: {
     flex: 1,
