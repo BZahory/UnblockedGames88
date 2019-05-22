4314560ebb73e843a12b22b4d362c82ef22afca5
bzahory <44588480+BZahory@users.noreply.github.com>
2019-05-22 12:35:52 -0400

    Add acceleration to the MoveBall code

--- MoveBall.js
+++ MoveBall.js
@@ -1,33 +1,36 @@
 import React, { Component } from 'react';
 import { View, StyleSheet, Text, Dimensions } from 'react-native';
 
+Expo.ScreenOrientation.allow(Expo.ScreenOrientation.LANDSCAPE_RIGHT);
 
-//todo: make the ball expand and contract 
+//todo: make the ball expand and contract
 //      change the angle when the ball hits the wall - modify xSpeed or ySpeed
 class MoveBall extends React.Component {
 	constructor() {
        super();
-       this.state = { x: 50, 
-	                  y: 50,
+       this.state = { x: 50,
+	                  y: 205,
 					  xInc: true,
 					  yInc: true,
 					  xSpeed: 5,
 					  ySpeed: 15,
+						yAccel: 2,
 					  diameter: 60,
                       seconds: 0};
 	}
-	
+
 	timerEvent = () => {
 		//get the dimensions of the screen
 		let deviceWidth = Dimensions.get('window').width;
 		let deviceHeight = Dimensions.get('window').height;
-		
+
 		//update the current x coordinates
 		let curX = this.state.x;
 		let curXDir = this.state.xInc;
 		if (curXDir) {
+			
 			curX += this.state.xSpeed;
-			if (curX > deviceWidth-this.state.diameter) {
+			if (curX > deviceWidth-this.state.diameter-20) {
 				curXDir = false;
 			}
 		}
@@ -37,30 +40,28 @@ class MoveBall extends React.Component {
 				curXDir = true;
 			}
 		}
-		
+
 		//update the current y coordinates
 		let curY = this.state.y;
 		let curYDir = this.state.yInc;
 		if (curYDir) {
+			this.state.ySpeed += this.state.yAccel;
 			curY += this.state.ySpeed;
-			if (curY > deviceHeight) {
-				curYDir = false;
+			if (curY > deviceHeight-100) {
+				this.state.ySpeed *= -1;
 			}
-		}
-		else  {
-			curY -= this.state.ySpeed;
 			if (curY < 0) {
-				curYDir = true;
+				this.state.ySpeed *= -1;
 			}
 		}
 		//update state with local variables
         this.setState( {x: curX, y: curY, xInc: curXDir, yInc: curYDir} );
     };
-  
+
   componentDidMount() {
-    setInterval( this.timerEvent, 20 );  
+    setInterval( this.timerEvent, 20 );
   }
-  
+
   ballStyle = function(options) {
      return {
       position: "absolute",
@@ -72,13 +73,13 @@ class MoveBall extends React.Component {
 	  backgroundColor: 'red',
      }
  }
-  
+
    render() {
       return (
 	    <View style={styles.container}>
 		  <View style={styles.timerView}>
              <Text style = {styles.textCenter}>
-                  x: {round(this.state.x)} y: {round(this.state.y)} 
+                  x: {round(this.state.x)} y: {round(this.state.y)}
               </Text>
           </View>
 		  <View style={this.ballStyle()}>
@@ -103,7 +104,7 @@ const styles = StyleSheet.create({
     flex: 2,
     alignItems: 'center',
     justifyContent: 'center',
-    
+
   },
   buttonView: {
     flex: 1,
